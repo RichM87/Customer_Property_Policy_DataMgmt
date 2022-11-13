bbed5cffc12fd8d2a142938a167640f85ca85ea3
Rich McDonald <105609362+RichM87@users.noreply.github.com>
2022-11-13 15:07:44 -0500

    Add files via upload

---
+++ InsuranceDataProject/HomeInsCSharpCodeFiles/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data.SqlClient;
+
+namespace _2ndCSharpSQLProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the PropertyID: ");
+            var propID = int.Parse(Console.ReadLine());
+            RetrievePropDetails(propID);
+        }
+
+        private static string InsConnection()
+        {
+            var connectionString = @"C:\Users\richm\source\repos\HomeInsCSharpCodeFiles\insConnectionString.TXT";
+            using (FileStream fs = File.OpenRead(connectionString))
+            {
+                using (StreamReader str = new StreamReader(fs))
+                {
+                    return str.ReadLine();
+                }
+            }
+        }
+        private static void RetrievePropDetails(int propID)
+        {
+            SqlConnection insConnect = new SqlConnection(InsConnection());
+            SqlCommand propCommand = new SqlCommand("SELECT * FROM Property WHERE PropID =" + propID, insConnect);
+
+            propCommand.Connection.Open();
+
+            SqlDataReader tableReader;
+
+            try
+            {
+                tableReader = propCommand.ExecuteReader();
+
+                if (tableReader.Read())
+                {
+                    var result = new Object[tableReader.FieldCount];
+                    Console.WriteLine(result);
+                    int numFields = tableReader.GetValues(result);
+                    Console.WriteLine(numFields);
+
+                    Console.WriteLine("The data for this property is as follows: ");
+                    foreach (var item in result)
+                    {
+                        Console.Write(item + "|");
+                    }
+                }
+                
+            }
+            catch
+            {
+                Console.WriteLine("There was a problem attempting the SELECT statement.");
+            }
+
+            propCommand.Connection.Close();
+        }
+    }
+}
